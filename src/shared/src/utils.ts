8d061b10c92c3bd7b2839af88d265f811068eccc
Wesley <wesleyvc2110@gmail.com>
2023-07-18 16:49:11 -0300

    perf: put verify functions in utils file

--- src/shared/src/utils.ts
+++ src/shared/src/utils.ts
@@ -694,3 +694,24 @@
 
 //   return false;
 // };
+
+export function verifyIfNotANumber(param: string): number {
+    const number = Number(param)
+
+    if (!number) {
+        throw new Error("Algum campo inválido!");
+    }
+    
+    return number
+}
+
+export function verifyIfPastDate(day: number, month: number, year: number) {
+    const dateActual = new Date();
+    const dayActual = dateActual.getDate();
+    const monthActual = dateActual.getMonth() + 1;
+    const yearActual = dateActual.getFullYear();
+
+    if (year < yearActual || year == yearActual && month < monthActual || year == yearActual && month == monthActual && day < dayActual){
+        throw new Error ("Informe uma data válida.");
+    }
+}
